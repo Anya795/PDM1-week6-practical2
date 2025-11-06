2ec9b87805f0deb23af2944a23ed43114c96e7b7
Anya795 <psd540@york.ac.uk>
2025-11-06 11:48:41 +0000

    Update sketch.js

--- exercise1-1/sketch.js
+++ exercise1-1/sketch.js
@@ -1,3 +1,29 @@
+class Star {
+    x;
+    y;
+    speedx;
+    speedy;
+    constructor(){
+        this.x = x;
+        this.y = y;
+        this.speedx = this.speedx
+        this.speedy = this.speedy
+    }
+}
+let myStar = new star(100, 300);
+
+myStar.draw();
+  draw();
+ fill(255, 234, 0);
+    noStroke();
+    triangle(x, y - 50, x - 20, y, x + 20, y);
+    triangle(x - 50, y - 20, x, y - 20, x, y + 10);
+    triangle(x + 50, y - 20, x, y - 20, x, y + 10);
+    triangle(x - 20, y - 5, x, y + 10, x - 35, y + 30);
+    triangle(x, y + 10, x + 20, y - 5, x + 35, y + 30);
+
+
+
 function setup() {
     createCanvas(400, 400);
 }
@@ -12,11 +38,4 @@ function draw() {
  * @param {number} y 
  */
 function star(x, y) {
-    fill(255, 234, 0);
-    noStroke();
-    triangle(x, y - 50, x - 20, y, x + 20, y);
-    triangle(x - 50, y - 20, x, y - 20, x, y + 10);
-    triangle(x + 50, y - 20, x, y - 20, x, y + 10);
-    triangle(x - 20, y - 5, x, y + 10, x - 35, y + 30);
-    triangle(x, y + 10, x + 20, y - 5, x + 35, y + 30);
 }
